9475a0a2a787238ce5efc540e8a12705fe34e497
omarfurkan <omarfurkan92@gmail.com>
2023-07-17 15:54:54 +0600

    mail and footer seciton design done

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,69 @@ import React from "react";
 import "./featuredproperties.css";
 
 const FeaturedProperties = () => {
-  return <div></div>;
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          className="fpImg"
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Aparhotel State Miasto</span>
+        <span className="fpCity">Mardrid</span>
+        <span className="fpPrice">Starting from $145</span>
+        <div className="fpRating">
+          <button>8.7</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          className="fpImg"
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Aparhotel State Miasto</span>
+        <span className="fpCity">Mardrid</span>
+        <span className="fpPrice">Starting from $145</span>
+        <div className="fpRating">
+          <button>8.7</button>
+          <span>Exallent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          className="fpImg"
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Aparhotel State Miasto</span>
+        <span className="fpCity">Mardrid</span>
+        <span className="fpPrice">Starting from $145</span>
+        <div className="fpRating">
+          <button>8.7</button>
+          <span>Exallent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          className="fpImg"
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Aparhotel State Miasto</span>
+        <span className="fpCity">Mardrid</span>
+        <span className="fpPrice">Starting from $145</span>
+        <div className="fpRating">
+          <button>8.7</button>
+          <span>Exallent</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProperties;
