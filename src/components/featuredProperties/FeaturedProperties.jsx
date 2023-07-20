fed6695a23ff4d95f3c1d4fc9c272432f0bf967d
omarfurkan <omarfurkan92@gmail.com>
2023-07-20 18:05:38 +0600

    fetch featured property

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,68 +1,34 @@
 import React from "react";
 import "./featuredproperties.css";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("hotels?featured=true");
+  console.log(data);
   return (
     <div className="fp">
-      <div className="fpItem">
-        <img
-          className="fpImg"
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Aparhotel State Miasto</span>
-        <span className="fpCity">Mardrid</span> <br />
-        <span className="fpPrice">Starting from $145</span>
-        <div className="fpRating">
-          <button>8.7</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-
-      <div className="fpItem">
-        <img
-          className="fpImg"
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Aparhotel State Miasto</span>
-        <span className="fpCity">Mardrid</span> <br />
-        <span className="fpPrice">Starting from $145</span>
-        <div className="fpRating">
-          <button>8.7</button>
-          <span>Exallent</span>
-        </div>
-      </div>
-
-      <div className="fpItem">
-        <img
-          className="fpImg"
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Aparhotel State Miasto</span>
-        <span className="fpCity">Mardrid</span> <br />
-        <span className="fpPrice">Starting from $145</span>
-        <div className="fpRating">
-          <button>8.7</button>
-          <span>Exallent</span>
-        </div>
-      </div>
-
-      <div className="fpItem">
-        <img
-          className="fpImg"
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Aparhotel State Miasto</span>
-        <span className="fpCity">Mardrid</span> <br />
-        <span className="fpPrice">Starting from $145</span>
-        <div className="fpRating">
-          <button>8.7</button>
-          <span>Exallent</span>
-        </div>
-      </div>
+      {loading ? (
+        "Loadign"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img className="fpImg" src={item?.photos[0]} alt="" />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span> <br />
+              <span className="fpPrice">
+                Starting from ${item.cheapestprice}
+              </span>
+              {item?.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
